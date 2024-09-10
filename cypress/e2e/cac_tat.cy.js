8e3c78605f922f9536507c33828f22ae45b8d512
Ines Melo <prof.inesmelo@gmail.com>
2024-09-09 22:40:52 -0300

    Cadastro de formulario em cypress

--- cypress/e2e/cac_tat.cy.js
+++ cypress/e2e/cac_tat.cy.js
@@ -1,12 +1,117 @@
 /// <reference types="Cypress" />
 
 const { visible } = require("ansi-colors")
+const { delay } = require("bluebird")
 
 describe('Central de Atendimento ao Cliente TAT', () => {
     beforeEach(() => {
         cy.visit('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
     })
-    it('verifica o título da aplicação', () => {
-        cy.title().sho
+    it('Validar o título da aplicação', () => {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('Validar os campos obrigatórios', () => {
+        cy.cadError()
+    })
+
+    it('Validar envio de formulário com sucesso', () => {
+        cy.cadFormSuccess()
+        cy.cadSuccess()
+    })
+
+    it('Validar envio de formulário com email inválido', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste#qa.com')
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadError()
+    })
+
+    it('Validar texto no campo Telefone', () => {
+        cy.cadPhoneInvalid('Teste', '')
+    })
+
+    it('Validar obrigatoriedade no campo Telefone', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+    it('Limpar os campos obrigatorios', () => {
+        cy.cadFirstName('Teste').clear().should('have.value', '')
+        cy.cadLastName('QA').clear().should('have.value', '')
+        cy.cadEmail('teste@qa.com').clear().should('have.value', '')
+        cy.cadPhone('21999999999').clear().should('have.value', '')
+        cy.cadPraise('Aqui será um teste de qualidade').clear().should('have.value', '')
+        cy.cadError()
+    })
+
+    it('Validar cadastro selecionando um Produto Youtube', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadProduct('youtube', 'youtube')
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+    it('Validar cadastro selecionando um Produto Mentoria', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadProduct('Mentoria', 'mentoria')
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+    it('Validar cadastro selecionando um Produto Blog', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadProduct(1, 'blog')
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+    it('Validar cadastro selecionando Atendimento Feedback', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadProduct(1, 'blog')
+
+        cy.get('input[type="radio"][value="feedback"]').should('be.visible')
+            .check()
+            .should('be.checked', 'feedback')
+
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+    it('Validar cadastro selecionando todos os Atendimentos', () => {
+        cy.cadFirstName('Teste')
+        cy.cadLastName('QA')
+        cy.cadEmail('teste@qa.com')
+        cy.cadPhone('21999999999')
+        cy.cadProduct(1, 'blog')
+
+        cy.get('input[type="radio"]').should('be.visible')
+            .should('have.length', 3)
+            .each(($radio) => {
+                cy.wrap($radio).check()
+            })
+            .check().should('be.checked', 'feedback')
+
+        cy.cadPraise('Aqui será um teste de qualidade')
+        cy.cadSuccess()
+    })
+
+
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,60 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('cadFirstName', (firstName) => {
+    cy.get('#firstName').should('be.visible').type(firstName, { delay: 0 }).should('have.value', firstName)
+})
+
+Cypress.Commands.add('cadLastName', (lastName) => {
+    cy.get('#lastName').should('be.visible').type(lastName, { delay: 0 }).should('have.value', lastName)
+})
+
+Cypress.Commands.add('cadEmail', (email) => {
+    cy.get('#email').should('be.visible').type(email, { delay: 0 }).should('have.value', email)
+})
+
+Cypress.Commands.add('cadPhone', (phone) => {
+    cy.get("label[for='phone']").should('be.visible', 'style="display: none;"')
+    cy.get('#phone-checkbox').check().should('be.checked')
+    cy.get("label[for='phone']").should("be.visible", 'style="display: inline;"', ' (obrigatório)')
+    cy.get("#phone").should('be.visible').type(phone, { delay: 0 }).should('have.value', phone)
+})
+
+Cypress.Commands.add('cadPhoneInvalid', (phone, resultPhone) => {
+    cy.get("label[for='phone']").should('be.visible', 'style="display: none;"')
+    cy.get('#phone-checkbox').check().should('be.checked')
+    cy.get("label[for='phone']").should("be.visible", 'style="display: inline;"', ' (obrigatório)')
+    cy.get("#phone").should('be.visible').type(phone, { delay: 0 }).clear().should('have.value', resultPhone)
+})
+
+Cypress.Commands.add('cadProduct', (value, options) => {
+    cy.get('#product').should('be.visible').select(value)
+        .should('have.value', options)
+})
+/*
+Cypress.Commands.add('cadAtend', (value, options) => {
+    cy.get('input[type="radio"][value=value]').should('be.visible').check(value)
+        .should('be.checked', options)
+})
+*/
+Cypress.Commands.add('cadPraise', (praise) => {
+    cy.get('#open-text-area').should('be.visible').type(praise, { delay: 0 }).should('have.value', praise)
+})
+
+Cypress.Commands.add('cadSuccess', () => {
+    cy.get('button[type="submit"]').should('be.visible').click()
+    cy.get('span[class="success"]').should('be.visible', 'Mensagem enviada com sucesso.')
+})
+
+Cypress.Commands.add('cadError', () => {
+    cy.get('button[type="submit"]').should('be.visible').click()
+    cy.get('span[class="error"]').should('be.visible', 'Valide os campos obrigatórios!')
+})
+
+Cypress.Commands.add('cadFormSuccess', () => {
+    cy.get('#firstName').should('be.visible').type('Teste', { delay: 0 }).should('have.value', 'Teste')
+    cy.get('#lastName').should('be.visible').type('QA', { delay: 0 }).should('have.value', 'QA')
+    cy.get('#email').should('be.visible').type('teste@qa.com', { delay: 0 }).should('have.value', 'teste@qa.com')
+    cy.get('#open-text-area').should('be.visible').type('Aqui será um teste de qualidade', { delay: 0 }).should('have.value', 'Aqui será um teste de qualidade')
+    cy.get('button[type="submit"]').should('be.visible').click()
+})
